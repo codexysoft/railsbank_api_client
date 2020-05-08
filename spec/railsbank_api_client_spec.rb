8e618566a172feb55c992a8da13bdb59d0c1506c
kolisnyklera <kolisnyklera@gmila.com>
2020-05-08 12:52:49 +0300

    Add gem config

---
+++ spec/railsbank_api_client_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RailsbankApiClient do
+  it 'has a version number' do
+    expect(RailsbankApiClient::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
